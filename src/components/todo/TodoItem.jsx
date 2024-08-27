ce480ce24abeb1f722c7f32fa520a3074da7dfc5
play93 <95739833+play93@users.noreply.github.com>
2024-08-27 15:47:47 +0900

    style: 투두아이템 스타일 추가

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,13 +1,62 @@
+import styled from "styled-components";
+
 const TodoItem = ({ todo, toggleCompleted, handleDelete }) => {
   return (
-    <li key={todo.id}>
-      <p>
-        {todo.text} - {String(todo.completed)}
-      </p>
-      <button onClick={() => toggleCompleted(todo.id)}>완료</button>
-      <button onClick={() => handleDelete(todo.id)}>삭제</button>
-    </li>
+    <TaskItem key={todo.id}>
+      <TaskItemContent>
+        <p>
+          {todo.text} - {String(todo.completed)}
+        </p>
+      </TaskItemContent>
+      <TaskItemActions>
+        <TaskItemActionButton
+          onClick={() => toggleCompleted(todo.id)}
+          color="#582be7"
+        >
+          완료
+        </TaskItemActionButton>
+        <TaskItemActionButton
+          onClick={() => handleDelete(todo.id)}
+          color="#f05656"
+        >
+          삭제
+        </TaskItemActionButton>
+      </TaskItemActions>
+    </TaskItem>
   );
 };
 
 export default TodoItem;
+
+const TaskItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: white;
+`;
+
+const TaskItemContent = styled.div``;
+
+const TaskItemActions = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TaskItemActionButton = styled.button`
+  color: white;
+  background-color: ${({ color }) => color};
+
+  padding: 0.4rem 1rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  &:hover {
+    opacity: 70%;
+  }
+`;
