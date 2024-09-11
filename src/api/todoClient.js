e020dfcfeb43f9b31047442a61108d43732244f2
play93 <95739833+play93@users.noreply.github.com>
2024-09-11 20:01:42 +0900

    feat : 투두 클라이언트 API 함수로 분리

--- src/api/todoClient.js
+++ src/api/todoClient.js
@@ -3,3 +3,44 @@ import axios from "axios";
 export const todoClient = axios.create({
   baseURL: "http://localhost:3000/todos",
 });
+
+// API함수 완성
+// 이름에 맞지 않는 내용 삭제
+// todoClient를 통해 요청을 보내는 것만 남김
+
+// 모든 투두리스트 가져오기
+export const getTodos = async () => {
+  const { data } = await todoClient.get("/");
+
+  return data;
+};
+
+// 하나의 투두 가져오기
+export const getTodoDetail = async (id) => {
+  const { data } = await todoClient.get(`/${id}`);
+
+  return data;
+};
+
+// 투두 생성
+export const postTodo = async (newTodoObj) => {
+  const { data } = await todoClient.post("/", newTodoObj);
+
+  return data; // db에 있는걸 업데이트 요청 (setState처럼)
+};
+
+// 투두 완료/미완료 토글
+export const toggleTodo = async (id, completed) => {
+  const { data } = await todoClient.patch(`/${id}`, {
+    completed,
+  });
+
+  return data;
+};
+
+// 투두 삭제
+export const deleteTodo = async (id) => {
+  const { data } = await todoClient.delete(`/${id}`);
+
+  return data;
+};
